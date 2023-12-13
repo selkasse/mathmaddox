de7498e653e51893e659c1c099959b14fc44536f
Sharif Elkassed <selkasse@gmu.edu>
2023-12-12 22:34:27 -0500

    disabled button logic working

--- math.js
+++ math.js
@@ -3,22 +3,24 @@ const OPERATORS = ["+", "-", "x"];
 
 const equationForm = document.getElementById("equationForm");
 const newButton = document.getElementById("newButton");
-const answerButton = document.getElementById("answerButton");
+let answerButton = document.getElementById("answerButton");
 let firstNumber = document.getElementById("firstNumber");
 let operator = document.getElementById("operator");
 let secondNumber = document.getElementById("secondNumber");
+let answer = document.getElementById("answer");
+let result = document.getElementById("result");
 
 function getRandomNumber(max) {
   return Math.floor(Math.random() * max);
 }
 
 function calculateEquation() {
-  if (operator.innerHTML === "x") {
-    return firstNumber.innerHTML * secondNumber.innerHTML;
-  } else if (operator.innerHTML === "-") {
-    return firstNumber.innerHTML - secondNumber.innerHTML;
+  if (operator.innerText === "x") {
+    return firstNumber.innerText * secondNumber.innerText;
+  } else if (operator.innerText === "-") {
+    return firstNumber.innerText - secondNumber.innerText;
   } else {
-    return parseInt(firstNumber.innerHTML) + parseInt(secondNumber.innerHTML);
+    return parseInt(firstNumber.innerText) + parseInt(secondNumber.innerText);
   }
 }
 
@@ -28,35 +30,52 @@ function formSetup() {
     e.preventDefault();
 
     // handle submit
-    let result = document.getElementById("result");
-
-    let answer = document.getElementById("answer");
     console.log("calculationEquation is: " + calculateEquation());
     if (answer.value) {
+      result.classList.remove("hide");
       if (calculateEquation() == answer.value) {
-        result.innerHTML = "you have destroyed me at my own game!   )=";
+        result.innerText = "you have destroyed me at my own game!   )=";
       } else {
-        result.innerHTML = "I win you stupid human!";
+        result.innerText = "I win you stupid human!";
       }
 
-      answerButton.classList.add("hidden");
-      newButton.classList.remove("hidden");
+      // answerButton.classList.add("remove");
+      answerButton.disabled = true;
+      answerButton.classList.remove("bg-blue-500");
+      answerButton.classList.remove("hover:bg-blue-700");
+      answerButton.classList.add("bg-gray-500");
+
+      newButton.disabled = false;
+      newButton.classList.add("bg-blue-500");
+      newButton.classList.add("hover:bg-blue-700");
+      newButton.classList.remove("bg-gray-500");
+      // newButton.classList.remove("hide");
     }
   });
 }
 
 function equationSetup() {
-  firstNumber.innerHTML = getRandomNumber(MAX_NUM + 1);
-  operator.innerHTML = OPERATORS[getRandomNumber(OPERATORS.length)];
-  secondNumber.innerHTML = getRandomNumber(MAX_NUM + 1);
+  firstNumber.innerText = getRandomNumber(MAX_NUM + 1);
+  operator.innerText = OPERATORS[getRandomNumber(OPERATORS.length)];
+  secondNumber.innerText = getRandomNumber(MAX_NUM + 1);
   answer.value = "";
-  result.innerHTML = "";
-  answerButton.classList.remove("hidden");
-  newButton.classList.add("hidden");
+  result.classList.add("hide");
+  // answerButton.classList.remove("remove");
+  answerButton.disabled = false;
+  answerButton.classList.add("bg-blue-500");
+  answerButton.classList.add("hover:bg-blue-700");
+  answerButton.classList.remove("bg-gray-500");
+
+  newButton.disabled = true;
+  newButton.classList.remove("bg-blue-500");
+  newButton.classList.remove("hover:bg-blue-700");
+  newButton.classList.add("bg-gray-500");
+
+  // newButton.classList.add("hide");
 }
 
 equationSetup();
 formSetup();
 
 newButton.addEventListener("click", equationSetup);
-newButton.classList.add("hidden");
+// newButton.classList.add("hide");
